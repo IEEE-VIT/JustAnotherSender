e29ebe1202b7ea0502d4dbeb6604ff6979f8ed40
Sarthak Pranesh <sarthak.pranesh2018@vitstudent.ac.in>
2019-12-05 12:21:20 +0530

    Updates to app.js

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const path = require("path")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static('public'))
-app.set('views', path.join(__dirname, '/template'));
-app.set('view engine', 'hbs');
 
 
 //importing routes
 const route = require("./routes/route.js")
 
 app.use(route)
-//default route
+
 app.use((req, res, next)=>{
-    res.render("index.hbs")
+    res.status(200).send({
+        apiStatus:6,
+        apiMsg: "API working",
+    })
 })
 
 const port = process.env.PORT || 8080
