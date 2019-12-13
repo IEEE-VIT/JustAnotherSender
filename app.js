20836fea1d4bbe25d3464b230634b10874c675b4
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2019-12-13 21:26:23 +0530

    Added cors

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser')
 const path = require("path")
 const cors = require("cors")
 
-// app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 
 //importing routes
